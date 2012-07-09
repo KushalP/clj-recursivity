53bb9535f2174e9ee7ad781268394ae5206ae2ac
wfaler <wille.faler@gmail.com>
2012-07-09 23:05:11 +0100

    added expectations

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject clj-recursivity "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]               
-  :plugins [[lein-swank "1.4.4"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [expectations "1.4.3"]]            
+  :plugins [[lein-swank "1.4.4"]
+  			[lein-expectations "0.0.5"]])
--- test/recursivity/core_test.clj
+++ test/recursivity/core_test.clj
@@ -1,45 +1,30 @@
 (ns recursivity.core-test
   (:use [recursivity.core])
-  (:use [clojure.test])
+  (:use [expectations])
   )
 
-(deftest calculates-avg
-  (is (= 6 (avg [3 5 10])))
-  (is (= 6 (avg '(3 5 10)))))
+(expect 6 (avg [3 5 10]))
+(expect 6 (avg '(3 5 10)))
 
-(deftest calculate-max
-  (is (= 23 (max-value '(10 5 3 15 23 12))))
-  )
+(expect 23 (max-value '(10 5 3 15 23 12)))
 
-(deftest calculate-min
-  (is (= 3 (min-value '(10 5 3 15 23 12)))))
+(expect  3 (min-value '(10 5 3 15 23 12)))
 
-(deftest calculate-median
-  (is (= 10 (median '(10 5 3 23 12))))
-  (is (= 11 (median '(10 5 3 15 23 12))))
-  (is (= 4 (median '(5 3))))
-  )
+(expect 10 (median '(10 5 3 23 12)))
+(expect 11 (median '(10 5 3 15 23 12)))
+(expect 4 (median '(5 3)))
 
-(deftest calc-variance
-  (is (= 21704 (variance '(600 470 170 430 300)))))
+(expect 21704 (variance '(600 470 170 430 300)))
 
-(deftest calc-std-dev
-  (is (= 2N (std-dev '(2 4 4 4 5 5 7 9))))
-  )
+(expect  2N (std-dev '(2 4 4 4 5 5 7 9)))
 
-(deftest calc-sample-std-dev
-  (is (= 427617987059879/200000000000000 (sample-std-dev '(2 4 4 4 5 5 7 9)))) ; appr 2.138089935299395
-  )
+(expect 427617987059879/200000000000000 (sample-std-dev '(2 4 4 4 5 5 7 9))) ; appr 2.138089935299395
 
-(deftest calc-truerange
-  (is (= 0.9100000000000037 (true-range 48.7 47.79 48.16)))
-  )
+(expect 0.9100000000000037 (true-range 48.7 47.79 48.16))
 
-(deftest calc-sigmoid
-  (is (= (rationalize 0.5) (sigmoid 0))))
+(expect (rationalize 0.5) (sigmoid 0))
 
-(deftest calc-gaussian
-  (is (= (rationalize 0.03401870545760999) (gaussian 73 6.2 66))))
+(expect  (rationalize 0.03401870545760999) (gaussian 73 6.2 66))
 
-(deftest calc-euclidian-dist
-  (is (= 20.518284528683193 (euclidian-dist [3 18 104 90]))))
+(expect 20.518284528683193 (euclidian-dist [[3 18] [104 90]]))
+
